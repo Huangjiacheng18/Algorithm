02f520babbc8ecac7dab09ca5b2a36b4dce933cf
huangjiacheng <1652640479@qq.com>
2019-06-29 23:36:57 +0800

    直接选择排序、直接插入排序、希尔排序、冒泡排序 顺序查找、二分查找、二叉排序树

---
+++ app/src/main/java/net/lzzy/algorithm/algorlib/BubbleSort.java
@@ -0,0 +1,25 @@
+package net.lzzy.algorithm.algorlib;
+
+
+public class BubbleSort  <T extends Comparable<? super T>>extends BaseSort<T> {
+    public BubbleSort(T[] items) {
+        super(items);
+    }
+
+    @Override
+    void Sort() {
+        for (int i=0;i<items.length-1;i++){
+            for (int j=0;j<items.length-1-i;){
+                if (bigger(items[j],items[j+1])){
+                    T temp=items[j];
+                    items[j]=items[j+1];
+                    items[j+1]=temp;
+                }
+            }
+
+        }
+
+    }
+
+
+}
